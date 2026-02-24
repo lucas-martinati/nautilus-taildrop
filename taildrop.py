bfadc454bbb80581ab6d5cdbb7878e724ed7169b
Lucas_M54 <lucasm54800@gmail.com>
2026-02-24 13:13:55 +0100

    refactor: Improve Taildrop menu performance with device caching, add user notifications for file transfers and errors, and remove file receiving functionality.

--- taildrop.py
+++ taildrop.py
@@ -1,136 +1,131 @@
 #!/usr/bin/env python3
-"""
-Taildrop support for Nautilus.
-"""
 import json
 import subprocess
-
-try:
-    from urllib import unquote
-except ImportError:
-    from urllib.parse import unquote
-
+import time
+from urllib.parse import unquote
 from gi.repository import Nautilus, GObject
 
-
 class Taildrop:
-    """
-    Wrappers around taildrop.
-    """
+    _devices_cache = []
+    _last_cache_time = 0
+    _CACHE_TTL = 15  # Menu opens instantly thanks to this cache
+
+    @classmethod
+    def get_devices(cls):
+        now = time.time()
+        if now - cls._last_cache_time < cls._CACHE_TTL and cls._devices_cache:
+            return cls._devices_cache
+
+        try:
+            process = subprocess.run(
+                ['/usr/bin/tailscale', 'status', '--json'], 
+                capture_output=True, 
+                text=True, 
+                check=False
+            )
+            
+            if process.returncode != 0:
+                error_msg = process.stderr.strip() if process.stderr else "Unknown error"
+                subprocess.Popen(['notify-send', '-i', 'dialog-error', 'Tailscale Error', f"Status error: {error_msg}"])
+                return cls._devices_cache
+                
+            status = json.loads(process.stdout)
+            items = []
+            
+            for _host, data in status.get('Peer', {}).items():
+                if data.get('HostName') == "funnel-ingress-node":
+                    continue
+                    
+                clean_name = data.get('DNSName', '').split('.')[0]
+                if not clean_name:
+                    clean_name = data.get('HostName', 'Inconnu')
+                    
+                os_name = data.get('OS', '')
+                is_online = data.get('Online', False)
+                status_icon = "🟢" if is_online else "🔴"
+                
+                items.append({
+                    'hostname': clean_name,
+                    'label': f"{status_icon} {clean_name} ({os_name})",
+                    'is_online': is_online
+                })
+                
+            # Sort items: Online first (not is_online = False = 0), then alphabetically
+            items.sort(key=lambda x: (not x['is_online'], x['hostname'].lower()))
+            
+            cls._devices_cache = items
+            cls._last_cache_time = now
+            return items
+
+        except Exception as e:
+            subprocess.Popen(['notify-send', '-i', 'dialog-error', 'Tailscale Error', f"Execution error: {str(e)}"])
+            return cls._devices_cache
+
     @staticmethod
-    def find_hosts():
-        """
-        Discover the tailscale hosts that are online.
-        """
-        process = subprocess.run(
-            ['tailscale', 'status', '--json'],
-            capture_output=True,
-            check=False
-        )
-        status = json.loads(process.stdout)
-        user_id = status['Self']['UserID']
-        items = []
-        for _host, data in status['Peer'].items():
-            if data['UserID'] != user_id:
-                continue
-            if data['HostName'] == "funnel-ingress-node":
-                continue
-
-            items.append({
-                'hostname': data['DNSName'],
-                'os': data['OS'],
-                'online': data['Online']
-            })
-
-        return items
-
-    @staticmethod
-    def send_file(path, host):
-        """
-        Invoke the tailscale binary to send a file.
-        """
-        subprocess.Popen(['tailscale', 'file', 'cp', path, host + ':'])
-
-    @staticmethod
-    def get_file(path='.'):
-        """
-        Invoke the tailscale binary to receive a file.
-        """
-        subprocess.Popen(['tailscale', 'file', 'get', path])
+    def send_files(paths, host):
+        if not paths:
+            return
+            
+        # Smart notification (1 file vs multiple files)
+        if len(paths) == 1:
+            filename = paths[0].split('/')[-1]
+            message = f"Sending '{filename}' to {host}..."
+        else:
+            message = f"Sending {len(paths)} files to {host}..."
+            
+        # 1. Single and immediate notification (does not block Nautilus)
+        subprocess.Popen(['notify-send', '-i', 'transfer', 'Tailscale', message])
+        
+        # 2. Instant batch sending in "Fire and forget" mode (no Thread)
+        cmd = ['/usr/bin/tailscale', 'file', 'cp'] + paths + [f"{host}:"]
+        subprocess.Popen(cmd)
 
 
 class TaildropMenuProvider(GObject.GObject, Nautilus.MenuProvider):
-    """
-    Menu Provider for Taildrop
-    """
     def __init__(self):
-        pass
-
-    @staticmethod
-    def callback_recv(_menu, directory):
-        """
-        Callback handler for receiving files.
-        """
-        filename = unquote(directory.get_uri()[7:])
-        Taildrop.get_file(filename)
-        # hack, stolen from:
-        # https://github.com/brunonova/nautilus-hide/blob/master/extension/nautilus-hide.py#L124
-        subprocess.Popen(['xdotool', 'key', 'F5'])
+        super().__init__()
 
     @staticmethod
     def callback_send(_menu, hostname, files):
-        """
-        Callback Handler for sending files to a host.
-        """
+        paths = []
         for file in files:
-            filename = unquote(file.get_uri()[7:])
-            Taildrop.send_file(filename, hostname)
+            location = file.get_location()
+            if location:
+                filepath = location.get_path()
+                if filepath:
+                    paths.append(filepath)
+                    
+        if paths:
+            Taildrop.send_files(paths, hostname)
 
     def get_file_items(self, files):
-        """
-        Right click context menu for a batch of files.
-        """
         top_menuitem = Nautilus.MenuItem(
-            name='TaildropMenuProvider::Devices',
-            label='Taildrop',
-            tip='',
-            icon=''
+            name='Taildrop::Main', 
+            label='Send with Tailscale', 
+            tip='Share via Taildrop',
+            icon='transfer'
         )
-
+        
         submenu = Nautilus.Menu()
         top_menuitem.set_submenu(submenu)
 
-        for idx, details in enumerate(Taildrop.find_hosts()):
-            if not details['online']:
-                continue
+        try:
+            devices = Taildrop.get_devices()
+        except Exception:
+            return []
 
-            sub_menuitem = Nautilus.MenuItem(
-                name='TaildropMenuProvider::Device%i' % idx,
-                label="%s - %s" % (details['hostname'], details['os']),
-                tip='',
-                icon=''
-            )
-            sub_menuitem.connect(
-                'activate',
-                TaildropMenuProvider.callback_send, details['hostname'], files
-            )
-            submenu.append_item(sub_menuitem)
+        if not devices:
+            return []
 
-        return (top_menuitem,)
+        for device in devices:
+            item = Nautilus.MenuItem(
+                name=f'Taildrop::Device_{device["hostname"]}', 
+                label=device['label']
+            )
+            item.connect('activate', TaildropMenuProvider.callback_send, device['hostname'], files)
+            submenu.append_item(item)
+
+        return [top_menuitem]
 
     def get_background_items(self, file):
-        """
-        Adds the context menu to a folder.
-        """
-        top_menuitem = Nautilus.MenuItem(
-            name='TaildropMenuProvider::BackgroundRecieve',
-            label='Taildrop Receive',
-            tip='',
-            icon=''
-        )
-        top_menuitem.connect(
-            'activate',
-            TaildropMenuProvider.callback_recv, file
-        )
-
-        return (top_menuitem,)
+        return []
